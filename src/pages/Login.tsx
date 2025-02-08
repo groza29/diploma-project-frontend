3e600b288707ccca37f5c561d7c08811fd021c61
Emanuel Groza <emanuel.groza@sap.com>
2025-02-08 16:24:47 +0200

    redesign login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,129 +1,157 @@
 import React, { useState } from 'react';
+import Alert from '@mui/material/Alert';
+import Swal from 'sweetalert2';
 
 const Login: React.FC = () => {
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [error, setError] = useState<string | null>(null);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string | null>(null);
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        try {
-            const response = await fetch('http://localhost:3000/login', {
-                method: 'POST',
-                headers: {
-                    "access-control-allow-origin": "*",
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    email,
-                    password,
-                }),
-            });
+    try {
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'access-control-allow-origin': '*',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
 
-            if (!response.ok) {
-                throw new Error('Invalid email or password');
-            }
+      if (!response.ok) {
+        throw new Error('Invalid email or password');
+      }
 
-            const data = await response.json();
-            console.log('Login successful:', data);
+      const data = await response.json();
+      console.log('Login successful:', data);
+      Swal.fire({
+        icon: 'success',
+        title: 'Login Successful!',
+        text: 'Welcome!',
+        timer: 2000,
+        timerProgressBar: true,
+        showConfirmButton: false,
+      });
 
-            // Redirect or perform other actions after successful login
-        } catch (err: any) {
-            setError(err.message);
-        }
-    };
+      // Redirect or perform other actions after successful login
+    } catch (err: any) {
+      setError(err.message);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: err.message || 'Something went wrong!',
+        draggable: true,
+        iconColor: '#0C969C',
+        customClass: {
+          confirmButton:
+            'bg-primary font-thin text-text py-2 rounded-md mt-2 mb hover:bg-selected transition hover:text-white',
+          title: 'font-thin',
+        },
+      });
+    }
+  };
+  const handleForgotPassword = async () => {
+    const { value: email } = await Swal.fire({
+      title: 'Email',
+      input: 'email',
+      inputPlaceholder: 'Your email',
+      showCancelButton: true,
+      confirmButtonText: 'Reset Password',
+      width: '25%',
+      customClass: {
+        confirmButton:
+          'bg-primary text-text py-2 rounded-md mt-2 mb hover:bg-selected transition hover:text-white font-thin',
+        cancelButton: 'bg-white text-text py-2 rounded-md mt-2 mb  transition font-thin',
+        title: 'text-left font-thin text-md',
+        input: 'w-4/5 placeholder-selected placeholder-opacity-45 focus:outline-none ',
+      },
+    });
+    if (email) {
+      Swal.fire(`Entered email: ${email}`);
+    }
+  };
 
-    return (
-        <>
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
-                        Login
-                    </h2>
-                </div>
-
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form onSubmit={handleSubmit} className="space-y-6">
-                        <div>
-                            <label
-                                htmlFor="email"
-                                className="block text-sm font-medium text-gray-900"
-                            >
-                                Email address
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    required
-                                    autoComplete="email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label
-                                    htmlFor="password"
-                                    className="block text-sm font-medium text-gray-900"
-                                >
-                                    Password
-                                </label>
-                                <div className="text-sm">
-                                    <a
-                                        href="#"
-                                        className="font-semibold text-indigo-600 hover:text-indigo-500"
-                                    >
-                                        Forgot password?
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="mt-2">
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    required
-                                    autoComplete="current-password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
-                                />
-                            </div>
-                        </div>
-
-                        {error && (
-                            <div className="text-red-600 text-sm text-center">{error}</div>
-                        )}
-
-                        <div>
-                            <button
-                                type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Sign in
-                            </button>
-                        </div>
-                    </form>
-
-                    <p className="mt-10 text-center text-sm text-gray-500">
-                        Not a member?{' '}
-                        <a
-                            href="/register"
-                            className="font-semibold text-indigo-600 hover:text-indigo-500"
-                        >
-                            Register Now!
-                        </a>
-                    </p>
-                </div>
+  return (
+    <div className="flex h-screen">
+      {/* Left Section - Login Form */}
+      <div className="w-2/5 flex items-center justify-center bg-background">
+        <div className="w-96 p-8 rounded-lg">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            {/* Email Input */}
+            <div>
+              <label htmlFor="email" className="block text-m font-thin text-text mb-2">
+                Email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="block w-full rounded-md bg-white px-4 py-4 border border-border text-base text-text focus:outline-none focus:ring-2 focus:selected placeholder-selected placeholder-opacity-45"
+              />
             </div>
-        </>
-    );
+            {/* Password Input */}
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-m font-thin text-text mb-2">
+                  Password
+                </label>
+              </div>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Password"
+                required
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="block w-full rounded-md bg-white px-4 py-4 border border-border text-base text-text focus:outline-none focus:ring-2 focus:selected placeholder-selected placeholder-opacity-45"
+              />
+              {/* Sign Up Link */}
+              <p className="mt-6 text-sm font-thin text-text">Don't have an account? </p>
+              <a
+                href="/register"
+                className="font-semibold  text-sm text-selected hover:text-selected text-opacity-60 "
+              >
+                Sign up here!
+              </a>
+            </div>
+            {/* Sign In Button */}
+            <button
+              type="submit"
+              className="w-full bg-primary font-thin text-text py-2 rounded-md mt-4 mb hover:bg-selected transition hover:text-white"
+            >
+              Sign In
+            </button>
+            <div>
+              <a
+                onClick={handleForgotPassword}
+                href="#"
+                className="text-sm text-selected hover:text-selected mt-9 text-opacity-60 underline"
+              >
+                Forgot password?
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      {/* Right Section - Image */}
+      <div className="w-3/5">
+        <img src="/images/login1.jpg" className="w-full h-full object-cover shadow-lg" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#031716',
+        'green': '#032F30',
+        'selected': '#0A7075',
+        'primary': '#0C969C',
+        'button':"#6BA3BE",
+        'text-secondary':"#274D60",
+        'header-background': "#acc3ce",
+        'background': "#f5f5f5",
+        'border': "#d9d9d9",
+        'green': "#3ba85a",
+        'red': "#c93636"
+      },
+    },
   },
   plugins: [],
 };
